f51626739d96cf390c197338fa6e26eaa2d8fe8b
Vineeth Voruganti <18vvoruganti.stem@gmail.com>
2017-03-12 23:05:15 -0400

    firebase implementation for interest forms

--- js/form.js
+++ js/form.js
@@ -1,129 +1,167 @@
-$(document).ready(function() {
+var config = {
+    apiKey: "AIzaSyBdQmrHaeIMd35gcXFz1-CN-hxXFiBeIJs",
+    authDomain: "stemhacks-website-signup-info.firebaseapp.com",
+    databaseURL: "https://stemhacks-website-signup-info.firebaseio.com",
+    storageBucket: "stemhacks-website-signup-info.appspot.com",
+    messagingSenderId: "93884964900"
+  };
+  firebase.initializeApp(config);
 
-// Variable to hold request
-var request;
+var database = firebase.database();
 
-// Bind to the submit event of our form
-$("#Attendees").submit(function(event){
+function attendeeSubmit() {
+    var name = $("#AttendeeName").val();
+    var email = $("#AttendeeEmail").val();
+    var school = $("#AttendeeSchool").val();
+    database.ref("/Attendee/").update({
+      [name]:{
+        "email":email,
+        "school":school
+      }
+    });
+}
 
-    // Abort any pending request
-    if (request) {
-        request.abort();
+function mentorSubmit() {
+  var name = $("#MentorName").val();
+  var email = $("#MentorEmail").val();
+  var organization = $("#MentorOrganization").val();
+  database.ref("/Mentor/").update({
+    [name]:{
+      "email":email,
+      "orginization":organization
     }
-    // setup some local variables
-    var $form = $(this);
+  });
 
-    // Let's select and cache all the fields
-    var $inputs = $form.find("input, select, button, textarea");
-
-    // Serialize the data in the form
-    var serializedData = $form.serialize();
-
-    // Let's disable the inputs for the duration of the Ajax request.
-    // Note: we disable elements AFTER the form data has been serialized.
-    // Disabled form elements will not be serialized.
-    $inputs.prop("disabled", true);
-
-    // Fire off the request to /form.php
-    request = $.ajax({
-        url: "https://script.google.com/macros/s/AKfycbz4UCeFQPQ3_45WDXZaqudzSP_XPohfoo4_Er63_XHeE4yGTkI/exec",
-        type: "post",
-        data: serializedData
-    });
-
-    // Callback handler that will be called on success
-    request.done(function (response, textStatus, jqXHR){
-        // Log a message to the console
-        console.log("Hooray, it worked!");
-        swal("Success!", "We will contact you soon", "success")
-    });
-
-    // Callback handler that will be called on failure
-    request.fail(function (jqXHR, textStatus, errorThrown){
-        // Log the error to the console
-        console.error(
-            "The following error occurred: "+
-            textStatus, errorThrown
-        );
-    });
-
-    // Callback handler that will be called regardless
-    // if the request failed or succeeded
-    request.always(function () {
-        // Reenable the inputs
-        $inputs.prop("disabled", false);
-    });
-
-    // Prevent default posting of form
-    event.preventDefault();
-});
-
-});
+}
 
 
 
-
-
-
-
-$(document).ready(function() {
-
-// Variable to hold request
-var request;
-
-// Bind to the submit event of our form
-$("#Mentors").submit(function(event){
-
-    // Abort any pending request
-    if (request) {
-        request.abort();
-    }
-    // setup some local variables
-    var $form = $(this);
-
-    // Let's select and cache all the fields
-    var $inputs = $form.find("input, select, button, textarea");
-
-    // Serialize the data in the form
-    var serializedData = $form.serialize();
-
-    // Let's disable the inputs for the duration of the Ajax request.
-    // Note: we disable elements AFTER the form data has been serialized.
-    // Disabled form elements will not be serialized.
-    $inputs.prop("disabled", true);
-
-    // Fire off the request to /form.php
-    request = $.ajax({
-        url: "https://script.google.com/macros/s/AKfycby5z5IBk96CzJyxAHjXgjKNO_-LEbWtQFQ_KPU6Xrnv9GmaQew/exec",
-        type: "post",
-        data: serializedData
-    });
-
-    // Callback handler that will be called on success
-    request.done(function (response, textStatus, jqXHR){
-        // Log a message to the console
-        console.log("Hooray, it worked!");
-        swal("Success", "We will contact you soon", "success");
-    });
-
-    // Callback handler that will be called on failure
-    request.fail(function (jqXHR, textStatus, errorThrown){
-        // Log the error to the console
-        console.error(
-            "The following error occurred: "+
-            textStatus, errorThrown
-        );
-    });
-
-    // Callback handler that will be called regardless
-    // if the request failed or succeeded
-    request.always(function () {
-        // Reenable the inputs
-        $inputs.prop("disabled", false);
-    });
-
-    // Prevent default posting of form
-    event.preventDefault();
-});
-
-});
+// $(document).ready(function() {
+//
+// // Variable to hold request
+// var request;
+//
+// // Bind to the submit event of our form
+// $("#Attendees").submit(function(event){
+//
+//     // Abort any pending request
+//     if (request) {
+//         request.abort();
+//     }
+//     // setup some local variables
+//     var $form = $(this);
+//
+//     // Let's select and cache all the fields
+//     var $inputs = $form.find("input, select, button, textarea");
+//
+//     // Serialize the data in the form
+//     var serializedData = $form.serialize();
+//
+//     // Let's disable the inputs for the duration of the Ajax request.
+//     // Note: we disable elements AFTER the form data has been serialized.
+//     // Disabled form elements will not be serialized.
+//     $inputs.prop("disabled", true);
+//
+//     // Fire off the request to /form.php
+//     request = $.ajax({
+//         url: "https://script.google.com/macros/s/AKfycbz4UCeFQPQ3_45WDXZaqudzSP_XPohfoo4_Er63_XHeE4yGTkI/exec",
+//         type: "post",
+//         data: serializedData
+//     });
+//
+//     // Callback handler that will be called on success
+//     request.done(function (response, textStatus, jqXHR){
+//         // Log a message to the console
+//         console.log("Hooray, it worked!");
+//         swal("Success!", "We will contact you soon", "success")
+//     });
+//
+//     // Callback handler that will be called on failure
+//     request.fail(function (jqXHR, textStatus, errorThrown){
+//         // Log the error to the console
+//         console.error(
+//             "The following error occurred: "+
+//             textStatus, errorThrown
+//         );
+//     });
+//
+//     // Callback handler that will be called regardless
+//     // if the request failed or succeeded
+//     request.always(function () {
+//         // Reenable the inputs
+//         $inputs.prop("disabled", false);
+//     });
+//
+//     // Prevent default posting of form
+//     event.preventDefault();
+// });
+//
+// });
+//
+//
+//
+//
+//
+//
+//
+// $(document).ready(function() {
+//
+// // Variable to hold request
+// var request;
+//
+// // Bind to the submit event of our form
+// $("#Mentors").submit(function(event){
+//
+//     // Abort any pending request
+//     if (request) {
+//         request.abort();
+//     }
+//     // setup some local variables
+//     var $form = $(this);
+//
+//     // Let's select and cache all the fields
+//     var $inputs = $form.find("input, select, button, textarea");
+//
+//     // Serialize the data in the form
+//     var serializedData = $form.serialize();
+//
+//     // Let's disable the inputs for the duration of the Ajax request.
+//     // Note: we disable elements AFTER the form data has been serialized.
+//     // Disabled form elements will not be serialized.
+//     $inputs.prop("disabled", true);
+//
+//     // Fire off the request to /form.php
+//     request = $.ajax({
+//         url: "https://script.google.com/macros/s/AKfycby5z5IBk96CzJyxAHjXgjKNO_-LEbWtQFQ_KPU6Xrnv9GmaQew/exec",
+//         type: "post",
+//         data: serializedData
+//     });
+//
+//     // Callback handler that will be called on success
+//     request.done(function (response, textStatus, jqXHR){
+//         // Log a message to the console
+//         console.log("Hooray, it worked!");
+//         swal("Success", "We will contact you soon", "success");
+//     });
+//
+//     // Callback handler that will be called on failure
+//     request.fail(function (jqXHR, textStatus, errorThrown){
+//         // Log the error to the console
+//         console.error(
+//             "The following error occurred: "+
+//             textStatus, errorThrown
+//         );
+//     });
+//
+//     // Callback handler that will be called regardless
+//     // if the request failed or succeeded
+//     request.always(function () {
+//         // Reenable the inputs
+//         $inputs.prop("disabled", false);
+//     });
+//
+//     // Prevent default posting of form
+//     event.preventDefault();
+// });
+//
+// });
